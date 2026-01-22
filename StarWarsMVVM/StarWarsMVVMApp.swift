3a4dc2a17d771482bf65698ef4ba17a2a71472c1
Paul McGrath <gingermcninja@gmail.com>
2026-01-22 15:06:01 -0500

    STAR-1 Initial project creation

---
+++ StarWarsMVVM/StarWarsMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsMVVMApp.swift
+//  StarWarsMVVM
+//
+//  Created by Paul McGrath on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
